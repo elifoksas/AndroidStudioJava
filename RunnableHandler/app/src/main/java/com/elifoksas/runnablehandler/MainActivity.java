89e2a62da6a8eb82ca35e78d0b8d12fae173f5e5
elifoksas <elifoksas0@gmail.com>
2022-03-10 23:27:39 +0300

    RunnableHandler

---
+++ RunnableHandler/app/src/main/java/com/elifoksas/runnablehandler/MainActivity.java
@@ -0,0 +1,41 @@
+package com.elifoksas.runnablehandler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    int number;
+    Runnable runnable;//runnable-> bir işlemi birden fazla kez belirttiğimiz periyotta yapmamızı sağlar.
+    Handler handler;//handler-> runnable'ı çalıştırmamızı sağlar.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.textView);
+        number=0;
+
+    }
+    public void start(View view){
+
+        handler= new Handler();
+        runnable=new Runnable() {
+            @Override
+            public void run() {
+                textView.setText("Time: "+number);
+                number++;
+                textView.setText("Time: "+number);
+                handler.postDelayed(runnable,1000);//runnable'ı saniyede bir çalıştır.
+            }
+        };
+        handler.post(runnable);
+    }
+
+}
