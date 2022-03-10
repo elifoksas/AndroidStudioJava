5c582abe58c52713d9d12f9aa6d5a0b0172c5331
elifoksas <elifoksas0@gmail.com>
2022-03-10 16:52:26 +0300

    BilgiAktarımı

---
+++ MultipleActivitiesBilgiAktarımı/app/src/main/java/com/elifoksas/multipleactivities/MainActivity.java
@@ -0,0 +1,38 @@
+package com.elifoksas.multipleactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    String userName;
+    EditText editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userName="";
+
+        editText= findViewById(R.id.editText);
+    }
+
+    public void changeActivity(View view){
+
+        userName = editText.getText().toString(); //kullanıcıdan giriş alıyoruz.
+
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class); //olduğumuz sınıfı ve gitmek istediğimiz yeri yazıyoruz.
+
+        intent.putExtra("userInput",userName); //anahtar kelime ve yollanacak değer.
+
+        startActivity(intent);
+
+
+
+    }
+}
